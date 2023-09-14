b5f381de59caba537d8f83c121f4de31d06e12bc
ZHEN XUAN CHEONG <cheongzhenxuan19982@gmail.com>
2023-09-14 17:17:10 +0800

    created log functions

--- FT_WebAPIAjiya/Controllers/AjiyaActionController.cs
+++ FT_WebAPIAjiya/Controllers/AjiyaActionController.cs
@@ -7,18 +7,33 @@ using Microsoft.Data.SqlClient;
 using System.Data;
 using System.Dynamic;
 using Microsoft.AspNetCore.Authorization;
+using FT_WebAPIAjiya.Helpers;
 
 namespace FT_WebAPIAjiya.Controllers
 {
 
     public class AjiyaActionController : Controller
     {
+        private string logFileTitle { get; set; }
+        private string logFilePath { get; set; }
+        private Helpers.AjiyaLogFunction writeLog { get; set; }
 
         private readonly IConfiguration configuration;
 
         public AjiyaActionController(IConfiguration configuration)
         {
             this.configuration = configuration;
+            
+            //get the log file path from appsettings.json
+            logFileTitle = configuration.GetSection("AjiyaLogFile").GetValue<string>("LogFileTitle");
+            logFilePath = configuration.GetSection("AjiyaLogFile").GetValue<string>("LogFilePath");
+
+            writeLog = new AjiyaLogFunction
+            {
+                logFileTitle = logFileTitle,
+                logFilePath = logFilePath
+            };
+
         }
         [Authorize]
         [HttpPost]
@@ -26,15 +41,18 @@ namespace FT_WebAPIAjiya.Controllers
         public IActionResult AjiyaAction(string mytype, [FromBody] List<object> myobjects)
         {
             string conn_str = configuration.GetConnectionString("Default");
-            
+            writeLog.WriteLog($"Log", $"Web API started", $"SENDING");
+            string myjson = JsonConvert.SerializeObject(myobjects);
+
             using (var conn = new SqlConnection(conn_str))
             {
                try
                 {
-                    string myjson = JsonConvert.SerializeObject(myobjects);
+                    
                     string query = "exec sp_execaction @type, @json";
 
                     conn.Query(query, new { type = mytype, json = myjson });
+                    writeLog.WriteLog($"Log", $"sending Type: [{mytype}] JSON: {myjson}", $"INPUT");
 
                     //EXAMPLE JSON TO PARSE IN SWAGGER API: Format as following below:// dun miss the curly brackets
                     //[{"QuotationNo":"12312","CardCode":"C1231"}]
@@ -42,16 +60,14 @@ namespace FT_WebAPIAjiya.Controllers
                 }
                 catch (Exception ex) 
                 {
-                   
-                  return BadRequest(new {error= true ,errormessage = ex.Message });
+                    writeLog.WriteLog($"Log", $"send failed Type: [{mytype}] JSON: {myjson}", $"FAILED. Error: True, Error Message: {ex.Message}");
+                    return BadRequest(new {error= true ,errormessage = ex.Message });
                        
                 }
                 
 
             }
-
-            //return Ok($"Inserted {quotation.QuotationNo} successfully");
-            //return Ok($"Received {myobjects.Count} object ");
+            writeLog.WriteLog($"Log", $"send failed Type: {mytype} JSON: {myjson}", $"SUCCESS");
             return Ok(new { error = false, errormessage = "" });
         }
 
---
+++ FT_WebAPIAjiya/Helpers/AjiyaLogFunction.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace FT_WebAPIAjiya.Helpers
+{
+    public class AjiyaLogFunction
+    {
+        public string logFileTitle { get; set; }
+        public string logFilePath { get; set; }
+        public void WriteLog(string lvl, string msg, string msgStatus)
+        {
+            if (string.IsNullOrWhiteSpace(logFileTitle) || string.IsNullOrWhiteSpace(logFilePath)) return;
+
+            FileStream fileStream = null;
+            string filePath = "";
+            filePath = logFilePath;
+            //set the log file creation name
+            filePath = filePath + "[" + logFileTitle + "] Log_" + System.DateTime.Today.ToString("yyyyMMdd") + "." + "txt";
+
+            //create file 
+            FileInfo fileInfo = new FileInfo(filePath);
+            DirectoryInfo dirInfo = new DirectoryInfo(fileInfo.DirectoryName);
+
+            //check if file directory exist, if not create a new text file
+            if (!dirInfo.Exists) dirInfo.Create();
+
+            if (!fileInfo.Exists)
+            {
+                fileStream = fileInfo.Create();
+            }
+            else
+            {
+                fileStream = new FileStream(filePath, FileMode.Append);
+            }
+
+            StreamWriter log = new StreamWriter(fileStream);
+            if (string.IsNullOrWhiteSpace(lvl)) lvl = "Log";
+
+            //write log line, the curly bracket numbers below represent the index of the parameters, basically how u want to arrange your log sequence
+            log.WriteLine("[{0}-{2}][{3}]{1}", lvl, msg, DateTime.Now.ToString("yyyyMMdd HH:mm:ss"), msgStatus);
+
+            log.Close();
+        }
+    }
+}
